5f2dc9582158b9a2f34b5e0bcd3668e9e9a1c376
Edson Mendes <edson.luiz.mendes@hotmail.com>
2023-06-30 14:46:05 -0300

    Compartilhamento de dados entre components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'estudo-angular';
+
+  username: string = 'Edson Mendes';
+
+  userData = {
+    email: 'lorem@email.com',
+    name: 'Lorem Ipsum',
+  };
 }
